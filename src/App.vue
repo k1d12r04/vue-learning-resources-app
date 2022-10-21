9e1208389c525edd2b83d8996c4f490721b68e6f
Kadir <k1d12r04@gmail.com>
2022-10-21 15:50:28 +0300

    Add StoredResource component

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <stored-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></stored-resource>
+  </ul>
+</template>
+
+<script>
+import StoredResource from './components/learning-resources/StoredResource.vue';
+export default {
+  components: {
+    StoredResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com/',
+        },
+      ],
+    };
+  },
+};
+</script>
